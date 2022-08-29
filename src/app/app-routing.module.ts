e391fb8b54995aaa31f8c0c9fa5b04437452b6ba
Edward Tanguay (windows) <edward@tanguay.info>
2022-08-29 11:26:48 +0200

    basic router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeesPageComponent } from './employees-page/employees-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'employees',
+    component: EmployeesPageComponent,
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
