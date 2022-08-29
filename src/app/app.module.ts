e0c69d42da06c09661645651a04a175988c7c094
Edward Tanguay (windows) <edward@tanguay.info>
2022-08-29 11:22:59 +0200

    minimalized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeesPageComponent } from './employees-page/employees-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeesPageComponent
   ],
   imports: [
     BrowserModule,
